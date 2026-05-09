ab5acc7ef00656c3c2bd2d93d794b3f39b5cd658
Doğukan <d_incirbolen@hotmail.com>
2026-05-10 00:01:32 +0300

    Update : Input update and bet table

--- Assets/_RouletteGame/Scripts/Roulette/RouletteViewMono.cs
+++ Assets/_RouletteGame/Scripts/Roulette/RouletteViewMono.cs
@@ -6,12 +6,21 @@ namespace RouletteGame.Scripts
 {
     public class RouletteViewMono : MonoBehaviour
     {
-        [SerializeField] private Transform rouletteWheelRectTransform;
-        [SerializeField] private Button spinButton;
-        [SerializeField] private float _rotationDuration;
-        
+        private readonly uint[] _rouletteNumbers = new uint[]
+        {
+            0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36
+        };
+
+        [SerializeField]
+        private Transform rouletteWheelRectTransform;
+
+        [SerializeField]
+        private Button spinButton;
+
+        [SerializeField]
+        private float _rotationDuration;
+
         private Tween _idleRotationTween;
-        
 
         private void OnEnable()
         {
@@ -34,13 +43,13 @@ namespace RouletteGame.Scripts
             {
                 return;
             }
-            
-            _idleRotationTween = rouletteWheelRectTransform.DoRotateBy(new Vector3(0, 0,360f), _rotationDuration, Ease.Linear).SetLoops(-1);
+
+            _idleRotationTween = rouletteWheelRectTransform.DoRotateBy(new Vector3(0, 0, 360f), _rotationDuration, Ease.Linear).SetLoops(-1);
         }
 
         private void SetSlotTransform(GameObject slot, int i, float height)
         {
-           
+            
         }
     }
 }
---
+++ Assets/_RouletteGame/Scripts/Utilities/InputHandler.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace _RouletteGame.Utilities
+{
+    public class InputHandler : MonoBehaviour
+    {
+        [SerializeField] private LayerMask _clickableLayer;
+        [SerializeField] private float _tableHeight;
+
+        private Camera _mainCamera;
+        private Plane _tablePlane;
+        private readonly float _maxRayDistance = 200f;
+
+        private void Awake()
+        {
+            _mainCamera = Camera.main;
+            _tablePlane = new Plane(Vector3.up, new Vector3(0f, _tableHeight, 0f));
+        }
+
+        private void Update()
+        {
+            if (EventSystem.current.IsPointerOverGameObject())
+                return;
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                var clickPointOnWorld = GetRayHitPoint();
+                Debug.Log("X: " + clickPointOnWorld.x + 
+                          "\n Y:" + clickPointOnWorld.y);
+            }
+        }
+
+        private Vector2 GetRayHitPoint()
+        {
+            Ray ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (_tablePlane.Raycast(ray, out float distance))
+            {
+                Vector3 hitPoint = ray.GetPoint(distance);
+                var flatPosition = new Vector3(hitPoint.x, 0, hitPoint.z);
+                return flatPosition;
+            }
+
+            return Vector2.zero;
+        }
+    }
+}
